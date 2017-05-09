05060c75ed4de8942bf5f2aeb876d6072e34f6f7
Tom Fransson <itproffsettom@gmail.com>
2017-05-09 13:26:20 +0200

    Added a dummy variabel

--- src/com/jwhh/Main.java
+++ src/com/jwhh/Main.java
@@ -7,6 +7,7 @@ import java.io.Writer;
 public class Main {
 
     public static void main(String[] args) {
+        String dummyVariabel = "DummyDoh";
 	    //doTryCatchFinally();
 //        doTryWithResources();
 //        doTryWithResourcesMulti();
